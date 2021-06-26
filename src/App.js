e0d1f93cf146f46d3eb5e7e59aae02801b11f9bf
Pieter1254 <77567403+Pieter1254@users.noreply.github.com>
2021-06-26 07:14:20 -0700

    add files via Upload
    added src and public files.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Component } from 'react';
+import './App.css';
+import Userform  from './components/Userform';
+
+class App extends Component {
+    render () {
+       return (
+         <div className="App">
+           <Userform />
+         </div>
+       )
+    }
+}
+
+export default App;
